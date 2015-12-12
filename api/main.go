4089e5b7240b14234d0b5af20a7560a129b7886a
Justin Shattuck <justinshattuck@gmail.com>
2015-12-11 23:09:30 -0800

    starting to test bolt for in memory db

--- api/main.go
+++ api/main.go
@@ -3,9 +3,18 @@ package main
 import (
     "log"
     "net/http"
+
+    "github.com/boltdb/bolt"
 )
 
 func main() {
+	db, err := bolt.Open("db/hosts.db", 0644, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+
 	router := NewRouter()
 	
 	log.Fatal(http.ListenAndServe(":5000", router))
